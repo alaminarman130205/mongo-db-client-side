abcc497fb9c0770bff412c89e76dcfb397a19ef4
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-10-30 22:06:21 +0600

    third commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const users = useLoaderData();
+  const [displayUsers, setDisplayUsers] = useState(users);
+
   const handleDelete = (user) => {
     const agree = window.confirm(
       `are you sure you want to delete ${user.name} `
     );
     if (agree) {
-      console.log("deleting user with", user._id);
+      //   console.log("deleting user with", user._id);
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            alert("user deleted successfully ");
+            const remainingUsers = displayUsers.filter(
+              (usr) => usr._id !== user._id
+            );
+            setDisplayUsers(remainingUsers);
+          }
+        });
     }
   };
   return (
     <div>
-      <h1>home {users.length} </h1>
+      <h1>home {displayUsers.length} </h1>
       <div>
-        {users.map((user) => (
+        {displayUsers.map((user) => (
           <p key={user._id}>
             {user.name}
             <br />
